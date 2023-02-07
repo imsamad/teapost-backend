03e5623fa13777479a5db78cd7c4d75292d772f1
Abdus Samad <imsamad00@gmail.com>
2023-02-07 23:45:26 +0530

    added html for debug

--- src/lib/passport/google.ts
+++ src/lib/passport/google.ts
@@ -21,7 +21,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      callbackURL: 'http://teapost-backend.onrender.com/api/v1/auth/google_cb',
+      callbackURL: '/api/v1/auth/google_cb',
       scope: ['profile', 'email'],
     },
     async function (accessToken, refreshToken, { _raw, ...rest }, done) {
